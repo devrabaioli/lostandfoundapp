18ad4a910b1a01b2c0187ade0f9bc9d4bf2fc44c
devrabaioli <devrabaioli@gmail.com>
2023-01-02 09:14:51 +0000

    first commit

---
+++ src/main/java/devrabaioli/LadappApplication.java
@@ -0,0 +1,13 @@
+package devrabaioli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LadappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LadappApplication.class, args);
+	}
+
+}
